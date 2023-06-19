409df05891b35dd0f32e56bd16a6c57ab41f86f3
DiveshRW <divesh20july@gmail.com>
2023-06-19 23:42:03 +0530

    Project Setup along with Directory Structure and starter Boiler Plate

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 Invoice System which will be written in PHP and MySQL
 
 * The Project will have no PHP Frameworks.
-* Helps to Learn or Revise OOPS Concepts in PHP.
+* "Helps to learn or revise OOP concepts in PHP.
 
+## Requirements
 * PHP Version Used : 8.1
 * Mysql Database
+* Composer version 2.2.6
+* Apache/2.4.56
+
+# Folder Structure
+
+````
+* src
+* public
+	*aasets
+		*css
+ 	*index.php --The entry point		
+* views	
+* mysql
+	*sql
+	*schema.sql
+
+````
---
+++ views/layouts/app.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/4.6/examples/starter-template/starter-template.css" rel="stylesheet">
+
+</head>
+<body>
+  
+        
+<?php include_once 'menu.php'; ?>
+        
+
+
+<main role="main" class="container">
+
+  <div class="starter-template">
+    <h1>Invoice System</h1>
+    <p class="lead">In PHP with No Framework.<br>
+        <a target="_blank" href="https://php.net/">PHP official</a></p>
+  </div>
+
+</main><!-- /.container -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.6/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+</body>
+</html>
